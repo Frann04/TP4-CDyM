11314c2c1d42848b1f43a583c6151089a52e92f2
Franco Cazala <francocazala4@gmail.com>
2024-07-14 19:02:51 -0300

    Se corrigio frecuencia por software

--- Trabajo4/timer.c
+++ Trabajo4/timer.c
@@ -11,20 +11,20 @@
 void timer0_init(){    // Configuración del timer
 	TCCR0A=0x02; // Modo CTC
 	TCCR0B = (1 << CS01); // Prescaler de 8
-	OCR0A = 20;
+	OCR0A = 19;
 	TIMSK0=0x02; // Habilito Interrupción por comparación
 	sei();    // Se activan las interrupciones
 	
 
 }
 
-// Configuramos el timer con una frecuencia de 7812.5 Hz, mayor a los 50 Hz requeridos
+// Configuramos el timer con una frecuencia de 62500 Hz, mayor a los 50 Hz requeridos
 // Inicialmente configuaramos el comparadoer en 0, led apagado
 void timer1_init(){
-	OCR1A = 0;							// Blue
-	OCR1B = 0;							// Verde
+	OCR1A = 0;							// Contador color azul
+	OCR1B = 0;							// Contador color Verde
 	TCCR1A = (1<<COM1A1) | (1<<COM1B1) | (1<<WGM10);	// Modo 5 fast no invertido
-	TCCR1B =  (1<<WGM12) | (1<<CS10);		// prescaler 8
+	TCCR1B =  (1<<WGM12) | (1<<CS10);		// prescaler 1
 	
 	// Reiniciar contador a cero 
 	TCNT1 = 0;
